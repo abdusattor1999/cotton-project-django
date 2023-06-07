9acd93886a4c355a5559f0079d4d2d4b74b182f9
Abdusattor <abdusattor1999@gmail.com>
2023-06-07 19:44:26 +0500

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-ljw-$27ojh07!=2*vt28s!f=f&17w@j@s072=&z7p8(k+-eq&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
